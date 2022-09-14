23b89937b6613a49fecac2da2f47c9e811501f56
nikitak <788344@gmail.com>
2022-09-14 20:20:40 +0300

    tests+

--- LightJS.Tests/LjsTokenizerTest.cs
+++ LightJS.Tests/LjsTokenizerTest.cs
@@ -17,17 +17,45 @@ public class LjsTokenizerTest
           Assert.That(text, Is.Not.Empty);
      }
      
+     private static LjsTokenizer CreateLjsTokenizer(string scriptFileName)
+     {
+          var text = TestUtils.LoadJsFile(scriptFileName);
+
+          var ljsTokenizer = new LjsTokenizer(new LjsReader(text));
+          return ljsTokenizer;
+     }
+     
      [Test]
      public void ReadTokens_ShouldReturnTokens_WhenJsScriptIsValid()
      {
-          var text = TestUtils.LoadJsFile("simpleTest.js");
-
-          var ljsTokenizer = new LjsTokenizer(new LjsReader(text));
+          var ljsTokenizer = CreateLjsTokenizer("simpleTest.js");
 
           var tokens = ljsTokenizer.ReadTokens();
 
           tokens.Should().NotBeEmpty();
-     } 
+     }
+
+     
+
+     [Test]
+     public void ReadTokens_ShouldReturnTokensOfAllValidTypes_WhenJsScriptIsValid()
+     {
+          var ljsTokenizer = CreateLjsTokenizer("simpleTest.js");
+
+          var tokens = ljsTokenizer.ReadTokens();
+
+          var ljsTokenTypes = Enum.GetValues<LjsTokenType>().
+               Where(v => v != LjsTokenType.Null);
+
+          foreach (var tokenType in ljsTokenTypes)
+          {
+               tokens.Should().Contain(x => x.TokenType == tokenType);
+          }
+
+          tokens.Should().NotContain(x => x.TokenType == LjsTokenType.Null);
+     }
+     
+     // todo write tests to see how invalid scripts are handled
      
      // MethodName_Should_When
     
--- LightJS.Tests/js/simpleTest.js
+++ LightJS.Tests/js/simpleTest.js
@@ -2,12 +2,11 @@
 var x = 0
 var y = 0.1
 var xy = x + y * (x/y)
+var a = ["sdf",0,true]
 
 var f = function (a, b) {
     return a + b
 }
-
-
 
 function foo() {
     console.log("hello world!");
