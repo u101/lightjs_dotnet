2712faffb1f092750b72f377ed1e2285787e1af2
nikitak <788344@gmail.com>
2023-08-31 21:11:39 +0300

    LjsTypesConverter+

--- LightJS/Runtime/LjsTypesConverter.cs
+++ LightJS/Runtime/LjsTypesConverter.cs
@@ -8,7 +8,9 @@ public static class LjsTypesConverter
         LjsDouble i => i.Value == 0,
         LjsBoolean i => i.Value,
         LjsString i => !string.IsNullOrEmpty(i.Value),
-        _ => obj != LjsObject.Null && obj != LjsObject.Undefined
+        LjsNull _ => false,
+        LjsUndefined _ => false,
+        _ => true
     };
 
     public static int ToInt(LjsObject obj) => obj switch
@@ -24,6 +26,8 @@ public static class LjsTypesConverter
         LjsInteger i => i.Value,
         LjsDouble i => i.Value,
         LjsBoolean i => i.Value ? 1 : 0,
+        LjsNull _ => 0,
+        LjsUndefined _ => 0,
         _ => double.NaN
     };
     
