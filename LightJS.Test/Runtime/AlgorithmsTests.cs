cb963184ceef491f21cf5eba32d533846c221943
nikitak <788344@gmail.com>
2023-09-05 16:20:23 +0300

    AlgorithmsTests + merge sort with nested functions

--- LightJS.Test/Runtime/AlgorithmsTests.cs
+++ LightJS.Test/Runtime/AlgorithmsTests.cs
@@ -97,30 +97,32 @@ public class AlgorithmsTests
     {
         var code = """
         function mergeSort(arr) {
-          // Base case
-          if (arr.length <= 1) return arr
-          var mid = Math.floor(arr.length / 2)
-          // Recursive calls
-          var left = mergeSort(arr.slice(0, mid))
-          var right = mergeSort(arr.slice(mid))
-          return merge(left, right)
-        }
 
-        function merge(left, right) {
-          var sortedArr = [] // the sorted items will go here
-            
-          while (left.length && right.length) {
-            // Insert the smallest item into sortedArr
-            if (left[0] < right[0]) {
-              sortedArr.push(left.shift())
-            } else {
-              sortedArr.push(right.shift())
+            // Base case
+            if (arr.length <= 1) return arr
+            var mid = Math.floor(arr.length / 2)
+
+            // Recursive calls
+            var left = mergeSort(arr.slice(0, mid))
+            var right = mergeSort(arr.slice(mid))
+
+            return merge(left, right)
+
+            function merge(left, right) {
+                var sortedArr = [] // the sorted items will go here
+
+                while (left.length && right.length) {
+                    // Insert the smallest item into sortedArr
+                    if (left[0] < right[0]) {
+                        sortedArr.push(left.shift())
+                    } else {
+                        sortedArr.push(right.shift())
+                    }
+                }
+                // Use spread operators to create a new array, combining the three arrays
+                return sortedArr.concat(left,right);
             }
-          }
-          // Use spread operators to create a new array, combining the three arrays
-          return sortedArr.concat(left,right);
         }
-
         
         mergeSort([3, 5, 8, 5, 99, 1])
         """;
